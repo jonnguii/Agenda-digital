47ac923983dab48d7fe9bae1f1c19fca82e3f78f
jonnguii <jg.cravo@outlook.com>
2025-03-28 01:44:03 -0300

    Finalizando ajustes git ignore

--- src/main/java/com/muralis/desafio/Agenda_digital/model/TipoContato.java
+++
@@ -1,6 +0,0 @@
-package com.muralis.desafio.Agenda_digital.model;
-
-public enum TipoContato { //Tipo especial que determina campos padrões e evita que outro texto seja selecionado pelo usuario
-    Telefone,
-    Email
-}
